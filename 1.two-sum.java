8ba5c6306bbc4e79f1ac97c6d4afba349ce0232d
Baris Aydinoglu <baydinoglu@gmail.com>
2019-04-08 23:56:56 +0300

    7 Reverse Integer

--- 1.two-sum.java
+++ 1.two-sum.java
@@ -34,14 +34,14 @@ import java.util.*;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i=0, il=nums.length; i<il; i++) {
+        for (int i = 0, il = nums.length; i < il; i++) {
             Integer j = map.get(target - nums[i]);
             if (j != null) {
-                return new int[]{j, i};
+                return new int[] {j, i};
             }
             map.put(nums[i], i);
         }
-        throw new IllegalArgumentException("No two sum solution");        
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
 
