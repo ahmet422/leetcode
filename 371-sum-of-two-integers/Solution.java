4f66746f8295ef362b018c27c643bde4879edf2f
Baris Aydinoglu <baydinoglu@gmail.com>
2019-04-25 00:42:17 +0300

    371 Sum of Two Integers

---
+++ 371-sum-of-two-integers/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int getSum(int a, int b) {
+        if (a == 0) {
+            return b;
+        }
+        while (b != 0) {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
+    }
+}
